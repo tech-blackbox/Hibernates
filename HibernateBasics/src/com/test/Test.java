d082dbcf15ee2b3633ae90f19b2b565d39933d4b
Akash <akashtiwari2493@gmail.com>
2017-10-28 16:29:13 +0530

    Add a Test class file.

---
+++ HibernateBasics/src/com/test/Test.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubpublic class Test {
+
+		
+			System.out.println("Test my program.");
+		
+	}
+
+
+	}
+
+
